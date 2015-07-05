d619a22aa865699e19edbe4edd4af479290dbd48
Sukrit Chhabra <sukrit.chhabra@gmail.com>
2015-07-05 19:39:41 -0400

    Temporary html structure. Added more style to table. Toggle on tweet images

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 /*jslint browser: true*/
 /*jslint node: true*/
 /*global $, jQuery, alert*/
+
+$(document).ready(function() {
+    $('.linkOne').click(function(data) {
+        console.log(data);
+        $(this).siblings('.imgOne').slideToggle('fast');
+        $(this).siblings('.imgTwo').hide();
+    });
+
+    $('.linkTwo').click(function() {
+        $(this).siblings('.imgTwo').slideToggle('fast');
+        $(this).siblings('.imgOne').hide();
+    });
+});
