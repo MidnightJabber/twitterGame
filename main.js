c1ce9b51ea7dc28489de75bf402bf283ea42b30e
Sukrit Chhabra <sukrit.chhabra@gmail.com>
2015-07-06 11:49:24 -0400

    Click on .tweetCard tests whether click was also on .imgLink

--- main.js
+++ main.js
@@ -3,7 +3,8 @@
 /*global $, jQuery, alert*/
 
 $(document).ready(function() {
-    $('.linkOne').click(function(data) {
+    $('.linkOne').click(function (data) {
+        console.log("link: ");
         console.log(data);
         $(this).siblings('.imgOne').slideToggle('fast');
         $(this).siblings('.imgTwo').hide();
@@ -12,5 +13,22 @@ $(document).ready(function() {
     $('.linkTwo').click(function() {
         $(this).siblings('.imgTwo').slideToggle('fast');
         $(this).siblings('.imgOne').hide();
+    });
+
+    $('.tweetCard').click(function (event) {
+        console.log("\n\ntweetCard: " + event);
+        console.log((event.toElement.className));
+        console.log((event.toElement.className).indexOf('imgLink'));    //Detecting if click in tweetcard was on the imgLink class
+        
+
+        var clickedClassName = event.toElement.className;
+        if (clickedClassName.indexOf('imgLink') < 0 ) {
+            //click happened in tweetCard but not on image links
+            console.log('click happened in tweetCard but not on image links');
+        } else {
+            //click happened in tweetCard and on image links
+            console.log('click happened in tweetCard and on image links');
+        }
+
     });
 });
