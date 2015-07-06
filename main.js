f2284954f84c0ebe6417c1635132f3476f40f1b9
Vishrut Reddi <vishrutreddi@gmail.com>
2015-07-05 20:36:48 -0700

    Edited Front End, Added navbar, padding to table, bg-color
    * Added Images Folder (Logo will go here)
    
    * Added Nav-Bar in the Front-end.

--- main.js
+++ main.js
@@ -13,4 +13,13 @@ $(document).ready(function() {
         $(this).siblings('.imgTwo').slideToggle('fast');
         $(this).siblings('.imgOne').hide();
     });
+
+    // Start the Game Count-Down Timer
+    $('.countDownTimer').TimeCircles({ time: {
+	    Days: { color: "#C0C8CF", show: false},
+	    Hours: { color: "#C0C8CF", show: false },
+	    Minutes: { color: "#C0C8CF", show: false },
+	    Seconds: { color: "#1dcaff", show: true }
+	}}); 
+
 });
